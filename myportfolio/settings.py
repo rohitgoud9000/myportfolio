514075e59f9476cc31b0e8431838470bf1f6de7e
Rohit Goud <rohitgoud9000@gmail.com>
2025-06-21 21:04:38 +0530

    Final production setup: whitenoise, bcrypt, PostgreSQL env

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -103,6 +103,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Add this just above or below AUTH_PASSWORD_VALIDATORS
+
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.ScryptPasswordHasher',
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
